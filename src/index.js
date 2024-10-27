fbc2988500197d9ea47bb5393625030dca254fae
ritikrjk <ritikjain.ja@gmail.com>
2024-10-27 21:32:30 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path : './env'
+})
+
+
+connectDB()
+
+/*
+import express from "express";
+const app= express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", ()=>{
+      console.log("ERROR", error);
+    })
+
+    app.listen(process.env.PORT, ()=>{
+      console.log(`App is listneing on port ${process.env.PORT}`)
+    })
+  } catch (error) {
+    console.error("Error :", error);
+  }
+})();
+*/
